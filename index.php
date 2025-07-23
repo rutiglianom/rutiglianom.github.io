fb97a2804147f84f9701c3ff2eecd450fc485d6b
Matt Rutigliano <matthew.rutigliano@outlook.com>
2025-07-22 22:12:35 -0700

    Added other pages

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<link rel="stylesheet" href="style.css">
+		<title>About</title>
+	</head>
+	<body>
+		 <div class="topnav">
+		  <a class="active" href="#home">Home</a>
+		  <a href="Event.html">Event</a>
+		  <a href="WeddingParty.html">Wedding Party</a>
+		  <a href="TravelAccommodations.html">Travel & Accommodations</a>
+		  <a href="FAQ.html">FAQ</a>
+		  <a href="ThingsToDo.html">Things to do</a>
+		</div> 
+		<h1>Sararosa & Matthew</h1>
+		 <img
+		  	src="Images/HomepageMattSR.jpg" 
+		  	alt="The happy couple" width="500" 
+		  /> 
+	
+		<h2>Our Story</h2>
+		<p><?php include('Text/OurStory.txt'); ?></p>
+	
+	</body>
+</html> 
